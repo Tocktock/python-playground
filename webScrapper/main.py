ae3cb0d4f792892965633d6b5e5bc4eeeb7ae083
Tocktock <qnfmtm666@naver.com>
2023-09-30 22:36:56 +0900

    job 가져오는거 하다가 막혀서 다음에 하기로

--- webScrapper/main.py
+++ webScrapper/main.py
@@ -1,5 +1,4 @@
-from time import sleep
-
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
@@ -10,3 +9,15 @@ options.add_argument("--disable-dev-shm-usage")
 browser = webdriver.Chrome(options=options)
 browser.get("https://www.indeed.com/jobs?q=python&l=Remote&start=10")
 
+soup = BeautifulSoup(browser.page_source, "html.parser")
+print(soup.text)
+job_list = soup.find("ul", class_="eu4oa1w0")
+
+jobs = job_list.find_all('li', recursive=False)
+
+for job in jobs:
+    zone = job.find("div", class_="mosaic-zone")
+    if zone == None:
+        print("job li")
+    else:
+        print("mosaic li")
